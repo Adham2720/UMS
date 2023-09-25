89d2a8df624308c5cf576bd442dee51e8c8310fa
Adham <145895638+Adham2720@users.noreply.github.com>
2023-09-25 12:47:13 +0300

    /addrole

--- Db/models/roles.model.js
+++ Db/models/roles.model.js
@@ -1,13 +1,17 @@
 import {sequelize} from '../connection.js'
-import { DataTypes } from 'sequelize';
 import User from './User.model.js';
 
 const role = sequelize.define('role',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
     name:{
         type :DataTypes.STRING,
         allowNull:false,
     }
-
 })
 role.hasMany(User);
 export default role
