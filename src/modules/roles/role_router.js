e0070463fc1062433b7a141c5ecd0f53d8f847c3
Youssef Gad <101581357+YoussefAbdellatif@users.noreply.github.com>
2023-09-25 11:56:25 +0300

    updated Add Role API

--- src/modules/roles/role_router.js
+++ src/modules/roles/role_router.js
@@ -5,4 +5,10 @@ const router = Router();
 router.get('/getrole',(req,res)=>{
     MainController.getAllRecordsOf(RolesModel,res)
 })
+const isValidRole= (req,res)=>{
+    return req.body.name != null;
+}
+router.post('/addRole',(req,res)=>{
+    MainController.addRecord(RolesModel,req,res,isValidRole)
+})
 export default  router;
