2bdf4a9dfd9cd21f74985ba7b7474d8116c61104
Youssef Gad <101581357+YoussefAbdellatif@users.noreply.github.com>
2023-09-26 11:18:33 +0300

    فك الضيقة يارب

--- Db/connection.js
+++ Db/connection.js
@@ -1,11 +1,9 @@
 import { Sequelize, DataTypes } from 'sequelize';
-import { config } from 'dotenv';
-config();
-export const sequelize = new Sequelize(process.env.postgres_ums, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host:process.env.HOST,
-    port:process.env.DB_PORT,
+export const sequelize = new Sequelize('postgres_ums', 'postgres_ums_user','UwGUJmrW8QvFdmZj0PsuOgZlWlY8Gff5', {
+    host:'dpg-ck871gg8elhc73ebv7s0-a',
+    port:5432,
     dialect:'postgres',
-  });
+});
 
 export const connectDB = async () => {
   try {
